c777371a4e1d14bbc6612dd4b113107fcad41f1d
vaibhav2628 <vaibhavpatil2579@gmail.com>
2025-03-15 14:28:50 +0530

    resolved the error in app.jsx regarding the useEffect

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { Analytics } from "@vercel/analytics/react"
 import VenueGallery from './Components/Featured/VenueGallery'
 import Hero from './Components/Hero/Hero'
